721b5b5bb3c411dd326e274fd5ad4fe98d9f30b0
playdog-io <cqdxcs3@163.com>
2021-06-19 22:58:16 +0800

    5:26 复制粘贴的pallet模板

--- pallets/kitties/src/lib.rs
+++ pallets/kitties/src/lib.rs
@@ -11,3 +11,38 @@ type KittyIndex = u32;
 #[derive(Encode, Decode)]
 pub struct Kitty(pub [u8; 16]);
 
+
+// 以下是pallet常规的开发
+pub trait Trait: frame_system::Trait {
+
+}
+
+
+decl_storage! {
+    trait Store for Module<T: Trait> as kitties {
+
+    }
+}
+
+decl_error!{
+    pub enum Error for Module<T: Trait> {
+
+    }
+}
+
+decl_event!(
+    pub enum Event<T> where <T as frame_system::Trait>::AccountId, {
+
+    }
+)
+
+impl<T: Trait> Module<T> {
+
+}
+
+
+//测试用例
+#[cfg(test)]
+mod test {
+    use super::*;
+}
