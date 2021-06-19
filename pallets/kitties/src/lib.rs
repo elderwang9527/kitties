a8a898e5c8c1829dfb5502221808ce4109b813ec
playdog-io <cqdxcs3@163.com>
2021-06-19 22:47:40 +0800

    2-2 04:30

--- pallets/kitties/src/lib.rs
+++ pallets/kitties/src/lib.rs
@@ -1,7 +1,13 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use codec::{Encode, Decode};
+use frame_support::{decl_module,decl_storage, decl_event, decl_error, StorageValue, ensure, StorageMap, traits::Randomness, Parameter,traits::{ExistenceRequirement ,Get, Currency, ReservableCurrency}
+};
+use sp_io::hashing::blake2_128;
+use frame_system::ensure_signed;
+use sp_runtime::DispatchError;
+
+type KittyIndex = u32;
+#[derive(Encode, Decode)]
+pub struct Kitty(pub [u8; 16]);
+
