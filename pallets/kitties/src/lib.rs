ffb915c6b48bef80318766522b7d4119a70cbe5b
playdog-io <cqdxcs3@163.com>
2021-06-20 14:00:05 +0800

    insert kittyy

--- pallets/kitties/src/lib.rs
+++ pallets/kitties/src/lib.rs
@@ -54,11 +54,18 @@ decl_module! {
         let kitty_id = Self::next_kitty_id()?;   //找到新创建的kitty的id，这里定义一个方法来实现，因为找到下个id其它地方也可能会使用。比如breed时。
         let dna = Self::random_value(&sender);   //为kitty创建数据，即之前说的u8数组。创建方法需要些随机化的方式
         let kitty = Kitty(dna);                  //有了dna数据后需要存储到链上，此为存储的操作。首先根据dna创建这个数据结构。因为链上定义的type是Kitty（见29行），所以这里用Kitty实例化一个object。
+        Self::insert_kitty(&sender, kitty_id, kitty);  //实例化kitty后insert到map里去。所以这里定义了一个方法来实现。好处也是可重用，比如breed也有相同的操作。
     }
     }
 }
 
 impl<T: Trait> Module<T> {
+    fn insert_kitty(owner: &T::AccountId, kitty_id: KittyIndex, kitty: Kitty) {
+        Kitties::insert(kitty_id, kitty); //首先向Kitties这个map里面增加key，value数值对
+        KittiesCount::put(kitty_id + 1);  //因为新增加了一个kitty，count加1。
+        <KittyOwners<T>>::insert(kitty_id, owner); //最后把owner保存到链上
+    }
+
     fn next_kitty_id() -> sp_std::result::Result<T::KittyIndex, DispatchError>{    //根据现在已有的kitty数量来找到id。
 		let kitty_id = Self::kitties_count();
 		if kitty_id == T::KittyIndex::max_value() {  //因为index是用的u32类型，所以可能会存在越界问题。所以判断如果达到最大值则返回一个错误。并把错误定义到decl_error中。
