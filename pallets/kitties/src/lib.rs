c43ba8c8477e5c8c3cdb5d9a39ffa0b4d08f895e
playdog-io <cqdxcs3@163.com>
2021-06-20 11:12:59 +0800

    10:00

--- pallets/kitties/src/lib.rs
+++ pallets/kitties/src/lib.rs
@@ -13,14 +13,24 @@ pub struct Kitty(pub [u8; 16]);
 
 
 // 以下是pallet常规的开发
+
+//所有的pallet都需要用到system包含的trait
 pub trait Trait: frame_system::Trait {
+    type Event: From<Event<Self>> + Into<<Self as frame_system::Trait>::Event>;
+    //产生kitty的dna数据时需要一些随机的函数
+    type Randomness: Randomness<Self::Hash>;
 
 }
 
 
 decl_storage! {
     trait Store for Module<T: Trait> as kitties {
-
+        //用于存储kitty数据，key是它的id
+        pub Kitties get(fn kitties): map hasher(blake2_128_concat) T::KittyIndex => Option<Kitty>;
+        //记录到了第几个kitty
+        pub KittiesCount get(fn kitties_count): T::KittyIndex;
+        //kitty的owner
+        pub KittyOwners get(fn kitty_owner): map hasher(blake2_128_concat) T::KittyIndex => Option<T::AccountId>;
     }
 }
 
@@ -35,6 +45,16 @@ decl_event!(
 
     }
 )
+
+decl_module! {
+    pub struct Module<T: Trait> for enum Call where origin: T::Origin {
+        #[weight=0]
+        pub fn create(origin){  //create 一个kitty
+        let sender = ensure_signed(origin)?;    //对于所有可调用方法，首先就要判断它的签名
+        //找到新创建的kitty的id，这里定义一个方法来实现，因为找到下个id其它地方也可能会使用。比如breed时。
+    }
+    }
+}
 
 impl<T: Trait> Module<T> {
 
